088e0f8809c0e3addd8e928aafbe4185c4a78116
carlopezc <carlotalcd@gmail.com>
2025-02-03 11:33:35 +0100

    cambios en mensajes de error

--- main/main.c
+++ main/main.c
@@ -6,7 +6,7 @@
 /*   By: carlopez <carlopez@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 13:39:47 by carlopez          #+#    #+#             */
-/*   Updated: 2025/01/30 16:05:13 by carlopez         ###   ########.fr       */
+/*   Updated: 2025/02/03 11:32:46 by carlopez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,16 @@ int	main(int argc, char **argv)
 	t_list	**stack_b;
 	int		chunks;
 
-	if (argc < 2)
-		return (write(2, "Error\n", 6), -1);
+	if (argc == 1)
+		return (0);
 	stack_a = (t_list **)malloc(sizeof(t_list *));
 	if (!stack_a)
 		return (-1);
 	*stack_a = NULL;
 	if (!fill_stack_a(stack_a, argv, argc))
 		return (free_list(stack_a, NULL), write(2, "Error\n", 6), -1);
+	else if (argc == 2)
+		return (free_list(stack_a, NULL), 0);
 	fill_index(stack_a);
 	if (is_ordered(stack_a))
 		return (free_list(stack_a, NULL), 0);
@@ -37,6 +39,5 @@ int	main(int argc, char **argv)
 	if (chunks == -1)
 		return (free_list(stack_a, stack_b), 0);
 	sort(stack_a, stack_b, chunks);
-	free_list(stack_a, stack_b);
-	return (0);
+	return (free_list(stack_a, stack_b), 0);
 }
