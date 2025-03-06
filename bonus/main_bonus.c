7c0478cb62d4bbd59a30ce95ac456d994bb90012
carlopezc <carlotalcd@gmail.com>
2025-03-06 17:05:24 +0100

    gooddddd

--- bonus/main_bonus.c
+++ bonus/main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: carlopez <carlopez@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 11:50:58 by carlopez          #+#    #+#             */
-/*   Updated: 2025/03/06 13:40:54 by carlopez         ###   ########.fr       */
+/*   Updated: 2025/03/06 16:55:33 by carlopez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,16 +24,16 @@ int	main(int argc, char **argv)
 		return (-1);
 	*stack_a = NULL;
 	if (!fill_stack_a(stack_a, argv, argc))
-		return (free_list(stack_a), ft_printf("main Error\n"), -1);
+		return (free_list(stack_a), write(2, "sError\n", 7), -1);
 	else if (argc == 2)
 		return (free_list(stack_a), 0);
 	stack_b = (t_list **)malloc(sizeof(t_list *));
 	if (!stack_b)
 		return (free_list(stack_a), -1);
 	*stack_b = NULL;
-	if (check_and_make(stack_a, stack_b) == 0)
+	if (!check_and_make(stack_a, stack_b))
 		return (free_list(stack_a), free_list(stack_b), -1);
-	if (is_ordered(stack_a) && !(*stack_b))
+	if (is_ordered(stack_a) && !*stack_b)
 		return (free_list(stack_a), free_list(stack_b), write(1, "OK\n", 3), 0);
 	return (free_list(stack_a), free_list(stack_b), write(1, "KO\n", 3), -1);
 }
